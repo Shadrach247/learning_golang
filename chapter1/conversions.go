29ffd43ac34f4a9efe0a8bbff670a33fbb4da9a9
shadrach247 <soshadrach@gmail.com>
2022-11-21 13:45:59 +0100

    add comments

--- chapter1/conversions.go
+++ chapter1/conversions.go
@@ -2,7 +2,7 @@
 // same type. If they're not, you'll get an error when trying to run your code.
 
 // The same is true of assigning new values to variables. if the type of
-// value being assigned doesn't match the declared type of the variabl, you'll get an error.
+// value being assigned doesn't match the declared type of the variable, you'll get an error.
 
 // The solution is to use conversions, which let you convert a value from one type to
 // another type. You just provide the type you want to convert a value to, immediately
@@ -13,11 +13,12 @@ package main
 import (
 	"fmt"
 	"reflect"
-
 )
 
 func main() {
 	var myInt int = 2
+	//conversion from int to float64 and assignment with Golang short variable declaration.
 	result := float64(myInt)
+	//relect is a package. While TypeOf is a function of the reflect package.
 	fmt.Println(reflect.TypeOf(result))
 }
