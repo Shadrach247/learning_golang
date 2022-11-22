52074c4cb123a5144029dc84f591a34a2d56c339
shadrach247 <soshadrach@gmail.com>
2022-11-22 07:40:17 +0100

    update contents

--- chapter1/conversions.go
+++ chapter1/conversions.go
@@ -1,12 +1,8 @@
 // Math and comparison operations in Go require that the included values be of the
 // same type. If they're not, you'll get an error when trying to run your code.
-
-// The same is true of assigning new values to variables. if the type of
-// value being assigned doesn't match the declared type of the variable, you'll get an error.
-
-// The solution is to use conversions, which let you convert a value from one type to
-// another type. You just provide the type you want to convert a value to, immediately
-// followed by the value you want to convert in parentheses.
+// conversions let you convert a value from one type toanother type. 
+// You just provide the type you want to convert a value to, 
+// immediately followed by the value you want to convert in parentheses.
 
 package main
 
