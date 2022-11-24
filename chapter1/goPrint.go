d5d54ae946a23402e2cf9162b2672700d851349e
shadrach247 <soshadrach@gmail.com>
2022-11-24 13:32:20 +0100

    add improved files naming and structure

---
+++ chapter1/goPrint.go
@@ -0,0 +1,22 @@
+// We use these three functions to print output messages in Go programming.
+
+// fmt.Print()
+// fmt.Println()
+// fmt.Printf()
+
+// Note: All these functions are defined under the fmt package. 
+// So, we must import the fmt package before we can use these functions.
+
+// Program to illustrate fmt.Print() 
+
+package main
+
+// import fmt package
+import "fmt"
+
+func main() {
+
+  fmt.Print("Hello, ")
+  fmt.Print("World!")
+
+}
