101a8c7ae3bca909735b40cb5f3e57373841c0ac
shadrach247 <soshadrach@gmail.com>
2022-11-29 10:30:18 +0100

    update go input file

--- chapter1/goInput.go
+++ chapter1/goInput.go
@@ -2,20 +2,14 @@
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+)
 
 func main() {
-  var name string
-  var city string
-
-  // takes input value for name
-  fmt.Print("Enter your name: ")
-  fmt.Scan(&name, &city)
-
-  fmt.Printf("Name: %s\nCity: %s\n", name, city)
 
 /*  
-fmt.Scan(&name)
 Takes input value from the user and assigns it to the name variable.
 Go programming provides three different variations of the Scan() method:
 
@@ -24,36 +18,74 @@ fmt.Scanln()
 fmt.Scanf()
 */
 
-  // Go fmt.Scanln()
-var myName string
-var myAge int
-    
-// take name and age input
-fmt.Println("Enter your name and age:")
-fmt.Scanln(&myName, &myAge) 
-fmt.Printf("Name: %s\nAge: %d\n", myName, myAge)
+// var name, city string
+var name string
+var age int
+var temperature float32
+var sunny bool 
 
-var newName string
-var newAge int
+/*
+  // takes input value for name
+  fmt.Print("Enter your name: \n")
+  fmt.Scan(&name)
+  fmt.Println("Where do you live?: ")
+  fmt.Scan(&city)
 
-// take name and age input using format specifier
-  fmt.Println("Enter your name and age:")
-  fmt.Scanf("%s %d", &newName, &newAge) 
-  fmt.Printf("Name: %s\nAge: %d\n", newName, newAge)
+  fmt.Printf("Name: %s\nCity: %s\n", name, city)
+*/
 
+fmt.Print("Enter your name and age: ")
+// Go fmt.Scan
+n, err := fmt.Scan(&name, &age)
+if err != nil {
+  log.Fatal(err)
+}
 
-  var temperature float32
-	var sunny bool
-  
+fmt.Printf("scanned %d arguments\n", n)
+fmt.Printf("%s is %d years old\n", name, age)
+
+// Go fmt.Scanln
+  var newName string
+	var newAge int
+
+// Input name from the user
+	fmt.Print("Enter name: ")
+	fmt.Scanln(&newName)
+
+// Input age from the user
+	fmt.Print("Enter age: ")
+	fmt.Scanln(&newAge)
+
+// Print the entered values
+	fmt.Println("Name: ", newName)
+	fmt.Println("Age : ", newAge)
+
+  var num1 int
+	var num2 int
+	var sum int
+
+// Input numbers
+	fmt.Print("Enter first number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Enter second number: ")
+	fmt.Scanln(&num2)
+
+// Calculate sum
+	sum = num1 + num2
+// Printing the values & sum
+	fmt.Printf("%d + %d = %d\n", num1, num2, sum)
+
+// Go fmt.Scanf
 // take float input
-   fmt.Println("Enter the current temperature:")
-   fmt.Scanf("%g", &temperature)
+fmt.Println("Enter the current temperature:")
+fmt.Scanf("%g", &temperature)
   
 // take boolean input
-   fmt.Println("Is the day sunny?")
-   fmt.Scanf("%t", &sunny)
+fmt.Println("Is the day sunny?")
+fmt.Scanf("%t", &sunny)
   
-   fmt.Printf("Current temperature: %g\nIs the day Sunny? %t", temperature, sunny)
+fmt.Printf("Current temperature: %g\nIs the day Sunny? %t", temperature, sunny)
 
 /*
 Data Type	Format Specifier
@@ -61,5 +93,8 @@ integer	        %d
 float	        %g
 string	        %s
 bool	        %t
+
+Each code block will need to be run separately to get the expected output.
 */
+
 }
