b66967bb03262427bd11e7a5cfc348fbf610e010
shadrach247 <soshadrach@gmail.com>
2023-02-08 15:11:14 +0100

    update functions

---
+++ Functions/anon3.go
@@ -0,0 +1,22 @@
+// we can also return a value from an anonymous function
+
+// Program to return value from an anonymous function
+
+package main
+import "fmt"
+
+func main() {
+
+  // anonymous function
+  var sum = func(n1, n2 int) int {
+    sum := n1 + n2
+  
+    return sum
+  } 
+
+  // function call
+  result := sum(5, 3)
+
+  fmt.Println("Sum is:", result)
+
+}
