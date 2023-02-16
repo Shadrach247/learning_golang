358f4a2eccc9976b11f9b73655b2cf9783dd202f
shadrach247 <soshadrach@gmail.com>
2023-02-16 15:46:38 +0100

    update function

---
+++ Functions/anon4.go
@@ -0,0 +1,16 @@
+// Program to return the area of a rectangle
+
+package main
+import "fmt"
+
+func main() {
+
+  // anonymous function
+  area := func(length, breadth int) int {
+    return length * breadth
+  } 
+
+  // function call using variable name
+  fmt.Println("The area of rectangle is", area(6,9))
+
+}
